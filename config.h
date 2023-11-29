b6d4ff09ebabb77ac74f9aecb9f1035a54f3d5c8
Daniel Liu <daniel17liu@gmail.com>
2023-11-29 11:07:15 -0800

    Added font2 patch

--- config.h
+++ config.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cozette:pixelsize=12:antialias=false:autohint=true";
+static char *font = "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = {
+};
 static int borderpx = 8;
 
 /*
